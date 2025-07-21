1e19b2fe4810d8dc5cf115b167ad03fb0ceea716
ItDoublebaby <manolo.diaz@pjchile.com>
2025-07-21 12:25:22 -0400

    Revert "Update multi token"
    This reverts commit a337e4e683f4813181ec86dbd9e73bf94ff79ff7.

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { authenticateUser, handleAuthRedirect, login, redirectToAuthCode,authenticateMultipleReports,authenticateMultpipleRol } from '../controllers/authController.js';
+import { authenticateUser, handleAuthRedirect, login, redirectToAuthCode } from '../controllers/authController.js';
 
 const router = express.Router();
 router.post('/login', login);
@@ -10,12 +10,5 @@ router.get('/loginAD', redirectToAuthCode);
 
 // Ruta para manejar la redirección después de la autenticación y obtener el token
 router.get('/callback', handleAuthRedirect);
-
-//pruebas de autenticación múltiple de reportes Power BI
-// Ruta para autenticación múltiple de reportes Power BI
-router.post('/auth/multiple', authenticateMultipleReports);
-
-router.post('/auth/multipleRol', authenticateMultpipleRol);
-
 
 export default router;
